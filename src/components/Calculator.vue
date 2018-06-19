5ac45ea69032ee096b01ad215715e344143916cf
Bartlomiej Tyrpien <bartlomein123@gmail.com>
2018-06-19 19:26:49 -0400

    first commit

---
+++ src/components/Calculator.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="calculator">
+    <div class="display">0.000112</div>
+    <div>C</div>
+    <div>+/-</div>
+
+    <div>%</div>
+    <div>÷</div>
+    <div>7</div>
+    <div>8</div>
+    <div>9</div>
+    <div>*</div>
+    <div>4</div>
+    <div>5</div>
+    <div>6</div>
+    <div>-</div>
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+    <div>+</div>
+    <div>0</div>
+    <div>.</div>
+    <div>=</div>
+   </div>
+</template>
+
+<script>
+export default {
+
+  }
+
+</script>
+
+
+<style scoped>
+.calculator{
+  max-width: 50%;
+  font-size:40px;
+  display: grid;
+  grid-template-columns: repeat(4,1fr);
+  grid-auto-rows: minmax(50px, atuo)
+}
+.display{
+  grid-column: 1 / 5;
+  background-color:pink;
+}
+</style>
