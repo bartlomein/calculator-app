25752e59a13f226745c57cb6edf4c3e976f94d1c
Bartlomiej Tyrpien <bartlomein123@gmail.com>
2018-06-19 20:15:54 -0400

    started the functions

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,47 +1,79 @@
 <template>
   <div class="calculator">
-    <div class="display">0.000112</div>
-    <div>C</div>
-    <div>+/-</div>
+    <div class="display">{{current || 0}}</div>
+    <div @click="clear" class = "button">C</div>
+    <div @click="sign" class = "button">+/-</div>
 
-    <div>%</div>
-    <div>÷</div>
-    <div>7</div>
-    <div>8</div>
-    <div>9</div>
-    <div>*</div>
-    <div>4</div>
-    <div>5</div>
-    <div>6</div>
-    <div>-</div>
-    <div>1</div>
-    <div>2</div>
-    <div>3</div>
-    <div>+</div>
-    <div>0</div>
-    <div>.</div>
-    <div>=</div>
+    <div @click="percent" class = "button">%</div>
+    <div class = "button operator">÷</div>
+    <div class = "button">7</div>
+    <div class = "button">8</div>
+    <div class = "button">9</div>
+    <div class = "button operator">*</div>
+    <div class = "button">4</div>
+    <div class = "button">5</div>
+    <div class = "button">6</div>
+    <div class = "button operator">-</div>
+    <div class = "button">1</div>
+    <div class = "button">2</div>
+    <div class = "button">3</div>
+    <div class = "button operator">+</div>
+    <div class = "zero button">0</div>
+    <div class = "button">.</div>
+    <div class = "button operator">=</div>
    </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      current: '',
+    };
+  },
+  methods: {
+    clear() {
+      this.current = '';
+    },
+    sign(){
+      this.current = this.current.charAt(0) === '-' ? 
+        this.current.slice(1) : `- ${this.current}`;
+    },
+    percent(){
+      this.current = `${parseFloat(this.current)/100}`;
+    },
+    append(number){
+       this.current = `${this.current}${number}`
+    }
   }
-
+};
 </script>
 
 
 <style scoped>
-.calculator{
-  max-width: 50%;
-  font-size:40px;
+.calculator {
+  max-width: 30%;
+  margin: 0 auto;
+  font-size: 40px;
   display: grid;
-  grid-template-columns: repeat(4,1fr);
-  grid-auto-rows: minmax(50px, atuo)
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, atuo);
 }
-.display{
+.display {
   grid-column: 1 / 5;
-  background-color:pink;
+  background-color: #333;
+  color: white;
+}
+
+.zero {
+  grid-column: 1/3;
+}
+.button {
+  background-color: #eee;
+  border: 0.5px solid #999;
+}
+.operator {
+  background-color: orange;
+  color: white;
 }
 </style>
