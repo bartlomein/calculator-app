d2fdad29ff0618300d2ff608471b4355f155cbe6
Bartlomiej Tyrpien <bartlomein123@gmail.com>
2018-06-20 11:52:31 -0400

    added animations to button presses

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="calculator">
     <div class="display">{{current || '0'}}</div>
-    <div @click="clear" class="btn">C</div>
-    <div @click="sign" class="btn">+/-</div>
-    <div @click="percent" class="btn">%</div>
-    <div @click="divide" class="btn operator">÷</div>
-    <div @click="append('7')" class="btn">7</div>
-    <div @click="append('8')" class="btn">8</div>
-    <div @click="append('9')" class="btn">9</div>
-    <div @click="times" class="btn operator">x</div>
-    <div @click="append('4')" class="btn">4</div>
-    <div @click="append('5')" class="btn">5</div>
-    <div @click="append('6')" class="btn">6</div>
-    <div @click="minus" class="btn operator">-</div>
-    <div @click="append('1')" class="btn">1</div>
-    <div @click="append('2')" class="btn">2</div>
-    <div @click="append('3')" class="btn">3</div>
-    <div @click="add" class="btn operator">+</div>
-    <div @click="append('0')" class="btn zero">0</div>
-    <div @click="dot" class="btn">.</div>
-    <div @click="equal" class="btn operator">=</div>
+    <div @click="clear(), animation('clear')" id = "clear" class="btn">C</div>
+    <div @click="sign(), animation('plus-minus')" id = "plus-minus"  class="btn">+/-</div>
+    <div @click="percent(),animation('percent')" id = "percent"  class="btn">%</div>
+    <div @click="divide(), animation('divide')" id = "divide"  class="btn operator">÷</div>
+    <div @click="append('7'),animation('seven')" id = "seven" class="btn">7</div>
+    <div @click="append('8'),animation('eight')" id = "eight" class="btn">8</div>
+    <div @click="append('9'),animation('nine')" id = "nine" class="btn">9</div>
+    <div @click="times(), animation('times')" id = "times" class="btn operator">x</div>
+    <div @click="append('4'),animation('four')" id = "four" class="btn">4</div>
+    <div @click="append('5'),animation('five')" id = "five" class="btn">5</div>
+    <div @click="append('6'),animation('six')" id = "six" class="btn">6</div>
+    <div @click="minus(),animation('minus')" id = "minus" class="btn operator">-</div>
+    <div @click="append('1'),animation('one')" id = "one" class="btn">1</div>
+    <div @click="append('2'),animation('two')" id = "two" class="btn">2</div>
+    <div @click="append('3'),animation('three')" id = "three" class="btn">3</div>
+    <div @click="add(),animation('add')" id = "add" class="btn operator">+</div>
+    <div @click="append('0'),animation('zero')" id = "zero" class="btn zero">0</div>
+    <div @click="dot(),animation('dot')" id = "dot" class="btn">.</div>
+    <div @click="equal(),animation('equal')" id = "equal" class="btn operator">=</div>
   </div>
 </template>
 
@@ -82,7 +82,23 @@ export default {
         parseFloat(this.previous)
       )}`;
       this.previous = null;
-    }
+    },
+    animation(id){
+      let item = document.getElementById(id);
+      console.log(item)
+      item.classList.add("button-clicked");
+      setTimeout(() => {
+        this.removeAnimation(id);
+      }, 500);
+      
+      
+    },
+    removeAnimation(id){
+      let itemToRemove = document.getElementById(id);
+      itemToRemove.classList.remove("button-clicked");
+      console.log(itemToRemove);
+    },
+
   }
 }
 </script>
@@ -109,12 +125,12 @@ export default {
 .button-clicked{
 
   animation-name: button-grow;
-  animation-duration: 0.5s;
+  animation-duration: 0.2s;
 }
 
 @keyframes button-grow {
     from {transform: scale(1.0);}
-    to {transform: scale(1.2);}
+    to {transform: scale(1.1);}
     
 }
 .btn {
